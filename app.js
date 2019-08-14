c126157dff5d84ae42f664323bb1547c5f8c9588
Rafail1 <ya.rafon-92@ya.ru>
2019-08-14 17:23:35 +0300

    14

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const http = require('http');
-const express = require('express');
 const path = require('path');
-const app = express();
+
+const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-})
-const server = http.createServer(app);
+app.use(errorController.get404);
 
-app.listen(process.env.PORT || 3000);
+sequelize.sync().then(res => {
+    app.listen(3000);    
+});
